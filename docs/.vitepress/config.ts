f85e9de0529ce81ac0576ed41d785d2f16f60fbf
MilkyCandy <1269748930@qq.com>
2025-08-22 03:16:41 +0800

    新增首页布局

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,28 +1,48 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  title: "牛奶糖的STM32笔记",
-  description: "大块牛奶糖的STM32笔记",
-  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   lang: 'zh-CN',
-
-  base: '/stm32/',
+  title: '牛奶糖的笔记',
+  description: 'STM32 / Linux 技术笔记',
   cleanUrls: 'with-subfolders',
   lastUpdated: true,
+
+  // head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
 
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
-      { text: '基础篇', link: '/basic-gpio' }
+      { text: 'STM32', link: '/stm32/' },
+      { text: 'Linux', link: '/linux/' }
     ],
 
-    sidebar: [
-      {
-        text: '学习笔记',
-        items: [
-          { text: 'GPIO基础', link: '/basic-gpio' },
-        ]
-      }
-    ]
+
+    // logo: '/logo.png',
+
+    footer: {
+      message: '鲁ICP备2022022379号-1',
+      copyright: '© 2025 大块牛奶糖'
+    },
+
+    sidebar: {
+      // 针对 /stm32/ 下的页面
+      '/stm32/': [
+        {
+          text: 'STM32 学习笔记',
+          items: [
+            { text: 'GPIO 基础', link: '/stm32/basic-gpio' }
+          ]
+        }
+      ],
+
+      // 针对 /linux/ 下的页面
+      '/linux/': [
+        {
+          text: 'Linux 学习笔记',
+          items: [
+          ]
+        }
+      ]
+    }
   }
 })
--- docs/index.md
+++ docs/index.md
@@ -1,2 +1,20 @@
-# Hello World!
-测试 GitHub Actions 自动部署
+---
+layout: home
+hero:
+  name: "大块牛奶糖的笔记"
+  tagline: "STM32 / Linux / More…"
+  actions:
+    - theme: brand
+      text: 进入 STM32 笔记
+      link: /stm32/
+    - theme: alt
+      text: Linux（筹备中）
+      link: /linux/
+features:
+  - icon:
+    title: STM32
+    details: GPIO、外设、裸机、HAL 示例
+  - icon:
+    title: Linux
+    details: 即将上线
+---
